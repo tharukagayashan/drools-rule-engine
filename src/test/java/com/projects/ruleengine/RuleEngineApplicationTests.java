210cc427e9200ea0eb767931f4d9b5ae29dfba21
Tharuka Gayashan <tharuka.g@informaticsint.com>
2023-12-12 13:02:10 +0530

    Initial commit

---
+++ src/test/java/com/projects/ruleengine/RuleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.ruleengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
